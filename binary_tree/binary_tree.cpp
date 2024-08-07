db48e99e5412614f53eaf2a871e0abf3b10ec6c9
Amirhosein Moradi <168298901+rahaan-sharif@users.noreply.github.com>
2024-08-07 10:32:52 +0330

    Update binary_tree.cpp

--- binary_tree/binary_tree.cpp
+++ binary_tree/binary_tree.cpp
@@ -7,6 +7,7 @@ private:
 	int data;
 public:
 	bt* left, * right;
+	~bt() {}
 	void create(bt*& start, int item)
 	{
 		start = new bt;
@@ -53,14 +54,30 @@ public:
 	}
 	void del_tree(bt*& start)
 	{
-		cout << start->data << endl;
 		if (start == NULL)
 		{
 			return;
 		}
 		del_tree(start->left);
 		del_tree(start->right);
-		delete this;
+		cout << start->data << endl;
+		delete start;
+	}
+	int max(bt*& start, int ent=INT_MIN)
+	{
+		if (start == NULL)
+			return INT_MIN;
+		int a = max(start->left);
+		cout << "* " << a << endl;
+		int b = max(start->right);
+		cout << " * " << b << endl;
+		int max = a;
+		if (max < b)
+			max = b;
+		if (max > start->data)
+			return max;
+		else
+			return start->data;
 	}
 };
 
@@ -72,13 +89,18 @@ int main(void)
 	root->right->left->create(root->right->left, 2);
 	root->right->left->left->create(root->right->left->left, 20);
 	root->left->create(root->left, 3);
-
 	root->show();
-	cout << "height of root: " << root->height(root) << endl;
-	cout << "count_leaves: " << root->count_leaf(root) << endl;
+	cout << endl;
 
-	cout << "deleting: " << endl;
+	cout << "height of root: " << root->height(root) << endl;
+	cout << "count_leaves: " << root->count_leaf(root) << endl<<endl;
+
+	cout << "max: " << root->max(root) << endl << endl;
+
+
+	cout << "\n\ndeleting: " << endl;
 	root->del_tree(root);
+	cout << "-----\n";
 	root->show();
 
 	return 0;
